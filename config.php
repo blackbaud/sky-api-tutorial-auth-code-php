3a2d4d29a7c6c6e400f55d6548e67ff1925a12b8
bdjones2410 <brandon.jones@blackbaud.com>
2017-03-01 16:25:35 -0500

    removing config file

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('BASEPATH', __DIR__);
-define('AuthClientId', "8d07c5b4-d614-4633-82d8-4743011b2a17");
-define('AuthClientSecret', "QntSONBWPocUm6MUnH22SukJ1bK29flF+QdV321W5dQ=");
-define('AuthRedirectUri', "http://localhost:8888/token");
-define('AuthSubscriptionKey', 'a49d639fd78b4478a26ab677c14878fa');
-define('BBOAuthBaseURI', 'https://oauth2.sky.blackbaud.com');
