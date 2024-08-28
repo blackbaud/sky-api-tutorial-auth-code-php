73f34ac92c03568b8e4bbb3788b84d527b21267a
Nilesh Unde <Nilesh.ude@blackbaud.me>
2024-08-28 17:35:39 +0530

    PR Comment resolve.

--- includes/blackbaud/blackbaud.php
+++ includes/blackbaud/blackbaud.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once 'session.php';
-require_once join(DIRECTORY_SEPARATOR, array($_SERVER['DOCUMENT_ROOT'], 'config.php.sample'));
+require_once join(DIRECTORY_SEPARATOR, array($_SERVER['DOCUMENT_ROOT'], 'config.php'));
 require_once join(DIRECTORY_SEPARATOR, array('api', 'constituents.php'));
 require_once 'auth.php';
 require_once 'http.php';
